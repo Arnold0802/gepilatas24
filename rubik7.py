2f3a962a1ed8b45805a668d08c1291fe7cf11f7b
Arnold0802 <arnold8908@gmail.com>
2024-05-16 21:24:08 +0200

    9 részre osztás
    előző verzióból a cellákra osztás felhasználása

--- rubik7.py
+++ rubik7.py
@@ -42,7 +42,7 @@ def match_color(hue, saturation, value, color_bounds):
     return "unknown"  # If no color matches
 
 
-i=1
+i=3
 minimum_area = 100
 #for i in range (1,6):
 
@@ -110,3 +110,152 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 #mivel már nem a 9 négyzet színeinek átlagát veszem, hanem egy megadott területből veszek mintát, fölöslegessé vált a maszkolás, így kivettem ezeket a lépéseket
+
+
+gamma_corrected = adjust_gamma(cropped_image, gamma=1.9)
+
+cv2.imshow('Gamma Corrected', gamma_corrected)
+
+image = gamma_corrected # ezzel a szerkesztett képpel folytatom a feldolgozást
+
+# Kép dimenzióinak lekérdezése
+height, width = image.shape[:2]
+
+# Vízszintes vonalak
+for i in range(1, 3):
+    cv2.line(image, (0, i * height // 3), (width, i * height // 3), (0, 255, 0), thickness=2)
+
+# Függőleges vonalak
+for i in range(1, 3):
+    cv2.line(image, (i * width // 3, 0), (i * width // 3, height), (0, 255, 0), thickness=2)
+
+# Kép megjelenítése
+cv2.imshow('Divided Image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Kép mentése
+#cv2.imwrite('divided_image.jpg', image)
+
+
+cell_height = height // 3
+cell_width = width // 3
+margin_height = int(cell_height * 0.33) 
+margin_width = int(cell_width * 0.33)
+
+# Minden cella középső részének kivágása és feldolgozása
+cells = []
+for i in range(3):  # Sorok
+    for j in range(3):  # Oszlopok
+        cell = image[i*cell_height:(i+1)*cell_height, j*cell_width:(j+1)*cell_width]
+        cells.append(cell)
+
+#for cell in cells:
+#    cv2.imshow('cella', cell)
+#    cv2.waitKey(0)
+#    cv2.destroyAllWindows()
+
+
+
+for i in range(0,9):
+
+
+    print(i)
+    image = cells[i]
+    namestr= "cella"+str(i)
+    cv2.imshow(namestr,image)
+
+
+    height, width = image.shape[:2]
+
+    x = int(width/5)
+    y = int(height/5)
+    w = int(width/4)
+    h = int(height/4)
+
+    cropped_image = image[y:y+h, x:x+w]
+    cv2.imshow(f'Cropped Image{i}', cropped_image)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+    image = cropped_image
+    # Átlagos BGR szín kiszámítása
+    average_color_per_row = np.average(image, axis=0)
+    average_color = np.average(average_color_per_row, axis=0)
+    average_color = np.uint8(average_color)  # Konvertálás egész színekre
+    print("Átlagos szín (BGR):", average_color)
+    print("B: ",average_color[0],"G: ",average_color[1],"R: ",average_color[2])
+    b=average_color[0]
+    g=average_color[1]
+    r=average_color[2]
+
+    # Átlagos szín megjelenítése egy képen
+    average_image = np.zeros_like(image, np.uint8)
+    average_image[:] = average_color
+
+    #cv2.imshow('Average Color', average_image)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+
+    # Színek 
+    color_bounds_bgr = {
+        'white': ((120, 120, 120), (255, 255, 255)),
+        'yellow': ((0, 150, 255), (30, 255, 255)),
+        'orange': ((0, 100, 200), (50, 180, 255)),
+        'blue': ((120, 0, 0), (255, 100, 100)),
+        'red': ((0, 0, 120), (75, 75, 255)),
+        'green': ((0, 120, 0), (100, 255, 100))
+    }
+
+    #color_bounds_hsv = {
+    #'white': ((0, 0, 200), (180, 50, 255)),  
+    #'yellow': ((0, 100, 100), (45, 255, 200)),  
+    #'orange': ((0, 100, 100), (25, 255, 255)),  
+    #'blue': ((100, 100, 100), (130, 255, 255)),  
+    #'red': ((0, 50, 20), (5, 255, 255)),  
+    #'green': ((45, 100, 100), (75, 255, 255))  
+    #}
+
+
+
+    #hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    #average_hue = np.mean(hsv_image[:,:,0])
+    #average_saturation = np.mean(hsv_image[:,:,1])
+    #average_value = np.mean(hsv_image[:,:,2])
+    #average_hue = int(average_hue)
+    #average_saturation = int(average_saturation)
+    #average_value = int(average_value)
+
+
+
+    #average_color_hsv = match_color(average_hue, average_saturation, average_value, color_bounds_hsv)
+
+    detected_color1 = determine_color_bgr(average_color,color_bounds_bgr)
+    max_bgr=max(average_color)
+    min_bgr=min(average_color)
+    print("max, min:" , max_bgr, min_bgr)
+    detected_color="unknown"
+    if ((max_bgr - min_bgr) < 30) and (min_bgr>100):
+        detected_color = "white"
+    if (b<(g//3)) and ((r-g)>30) and (g>70):
+        detected_color = "orange"
+    if (b<45) and (r>180) and (g>180) and ((g-r<15) and ((r-g<15))):
+        detected_color ="yellow"
+    if(b>94) and (g<(b//1.8)) and (r<(b//2)) and ((g-r<50) and ((r-g<50))):
+        detected_color ="blue"
+    if(r>60) and (b<(r//2)) and (g<(r//3)):
+        detected_color ="red"
+    if (g>30) and (r<(g//2)) and (b<(g//3)):
+        detected_color ="green"
+    
+    
+    print("detected color in BGR_bounds is:", detected_color1)
+    print("detected color in BGR if-else is:", detected_color)
+    #print("HSV values: ",average_hue," ",average_saturation," ",average_value)
+    #print(f"The detected color in HSV is: {average_color_hsv}\n")
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
